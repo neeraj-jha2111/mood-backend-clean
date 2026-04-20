f3f2b7ea3feba2019fcb7a9be17216d29e63cf39
neeraj-jha2111 <neeraj2111bla@gmail.com>
2026-04-20 09:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask_cors import CORS
 import sendgrid
 from sendgrid.helpers.mail import Mail
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -39,4 +36,6 @@ def mood():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
